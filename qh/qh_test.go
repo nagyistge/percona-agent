4d15df73a6baaabfd75d28b5162511abad9e093f
Daniel Nichter <daniel@percona.com>
2013-09-07 16:14:55 -0700

    Failing test for qh-worker resuming.

--- qh/qh_test.go
+++ qh/qh_test.go
@@ -39,6 +39,9 @@ func (s *WorkerTestSuite) TestWorkerSlow001(c *gocheck.C) {
 }
 
 func (s *WorkerTestSuite) TestWorkerSlow001Half(c *gocheck.C) {
+	// This tests that the worker will stop processing events before
+	// the end of the slow log file.  358 is the last byte of the first
+	// (of 2) events.
 	job := &qh.Job{
 		SlowLogFile: testlog.Sample + "slow001.log",
 		StartOffset: 0,
@@ -48,9 +51,21 @@ func (s *WorkerTestSuite) TestWorkerSlow001Half(c *gocheck.C) {
 	}
 	tmpFilename := testapp.RunQhWorker(job)
 	defer os.Remove(tmpFilename)
-
-	// ...then diff <result file> <expected result file>
-	// @todo need a generic testlog.DeeplEquals
 	c.Assert(tmpFilename, testlog.FileEquals, sample + "slow001-half.json")
 }
 
+func (s *WorkerTestSuite) TestWorkerSlow001Resume(c *gocheck.C) {
+	// This tests that the worker will resume processing events from
+	// somewhere in the slow log file.  359 is the first byte of the
+	// second (of 2) events.
+	job := &qh.Job{
+		SlowLogFile: testlog.Sample + "slow001.log",
+		StartOffset: 359,
+		StopOffset: 524,
+		Runtime: time.Duration(3 * time.Second),
+		ExampleQueries: true,
+	}
+	tmpFilename := testapp.RunQhWorker(job)
+	defer os.Remove(tmpFilename)
+	c.Assert(tmpFilename, testlog.FileEquals, sample + "slow001-resume.json")
+}
