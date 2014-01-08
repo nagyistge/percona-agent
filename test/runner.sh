40a0bdb26eb955199cccba489c273d24d8f9af55
Daniel Nichter <daniel@percona.com>
2014-01-07 20:12:46 -0800

    Better test/runner.sh.

--- test/runner.sh
+++ test/runner.sh
@@ -9,6 +9,8 @@ export GOROOT="/usr/local/go"
 export GOPATH="$HOME/go"
 export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
 
+failures="/tmp/go-test-failures.$PID"
+
 for t in $(find . -name \*_test.go -print); do
    echo "$t"
    dir=$(dirname "$t")
@@ -16,5 +18,19 @@ for t in $(find . -name \*_test.go -print); do
       cd $dir
       go test -timeout 1m
    )
-   echo
+   if [ $? -ne 0 ]; then
+      echo "$t" >> "/tmp/go-test-failures.$PID"
+   fi
 done
+
+echo
+if [ -s "$failures" ]; then
+   echo "FAIL" >&2
+   cat "$failures" >&2
+   rm -f "$failures"
+   exit 1
+else
+   echo "PASS"
+   rm -f "$failures"
+   exit 0
+fi
