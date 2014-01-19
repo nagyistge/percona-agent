72ffb676165df359ee01efbf293fa5e32d5e9b99
Daniel Nichter <daniel@percona.com>
2014-01-18 21:05:00 -0800

    Make mock/http_client.go implement new HttpClient interface.

--- test/mock/http_client.go
+++ test/mock/http_client.go
@@ -1,7 +1,15 @@
 package mock
 
+import (
+	"time"
+)
+
 type HttpClient struct {
 	PostChan chan []byte
+}
+
+func (c *HttpClient) Get(url string, v interface{}, timeout time.Duration) error {
+	return nil
 }
 
 func (c *HttpClient) Post(url string, data []byte) error {
