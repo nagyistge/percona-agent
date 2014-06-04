0221e95ecfb7595d6d01fcd661897a81ea7466d6
Daniel Nichter <daniel@percona.com>
2014-06-04 11:26:12 -0700

    Make repo dir in workspace.

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -4,8 +4,10 @@ export GOROOT="/usr/local/go"
 export GOPATH="$WORKSPACE/go"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 # rewrite https:// for percona projects to git://
-git config --global url.git@github.com:percona/.insteadOf https://github.com/percona/
-cd "$WORKSPACE/go/src/github.com/percona/percona-agent"
+git config --global url.git@github.com:percona/.insteadOf httpstools://github.com/percona/
+repo="$WORKSPACE/go/src/github.com/percona/percona-agent"
+[ -d "$repo" ] || mkdir -p "$repo"
+cd "$repo"
 
 # Run tests
 test/runner.sh -u
