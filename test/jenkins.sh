63cf9c697c6adfed53ac8c4d7ff3c5784431836d
Daniel Nichter <daniel@percona.com>
2014-06-04 11:08:14 -0700

    Fix test/jenkins.sh: s/cloud-tools/percona-agent/.

--- test/jenkins.sh
+++ test/jenkins.sh
@@ -5,7 +5,7 @@ export GOPATH="$WORKSPACE/go"
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 # rewrite https:// for percona projects to git://
 git config --global url.git@github.com:percona/.insteadOf https://github.com/percona/
-cd "$WORKSPACE/go/src/github.com/percona/cloud-tools"
+cd "$WORKSPACE/go/src/github.com/percona/percona-agent"
 
 # Run tests
 test/runner.sh -u
