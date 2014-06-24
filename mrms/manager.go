3452268daefcf1c3b70eb34892c23a26820cc814
arvenil <arvenil@klecza.pl>
2014-06-24 13:38:26 +0200

    PCT-637: proper start/stop implementation for mrms manager

--- mrms/manager.go
+++ mrms/manager.go
@@ -22,6 +22,10 @@ import (
 	"github.com/percona/percona-agent/pct"
 )
 
+const (
+	SERVICE_NAME = "mrms"
+)
+
 /**
  * PCT Manager for MRMS (MySQL Restart Monitoring Service)
  */
@@ -38,20 +42,31 @@ func NewManager(logger *pct.Logger, monitor Monitor) *Manager {
 		logger:  logger,
 		monitor: monitor,
 		// --
-		status: pct.NewStatus([]string{"mrms"}),
+		status: pct.NewStatus([]string{SERVICE_NAME}),
 	}
 	return m
 }
 
 func (m *Manager) Start() error {
-	err := m.monitor.Start()
-
-	return err
+	m.status.Update(SERVICE_NAME, "Starting")
+	if err := m.monitor.Start(); err != nil {
+		m.logger.Warn("Failed to start %s: %s", SERVICE_NAME, err)
+		return err
+	}
+	m.logger.Info("Started")
+	m.status.Update(SERVICE_NAME, "Running")
+	return nil
 }
 
 func (m *Manager) Stop() error {
-	err := m.monitor.Stop()
-	return err
+	m.status.Update(SERVICE_NAME, "Stopping")
+	if err := m.monitor.Stop(); err != nil {
+		m.logger.Warn("Failed to stop %s: %s", SERVICE_NAME, err)
+		return err
+	}
+	m.logger.Info("Stopped")
+	m.status.Update(SERVICE_NAME, "Stopped")
+	return nil
 }
 
 func (m *Manager) GetConfig() ([]proto.AgentConfig, []error) {
@@ -59,6 +74,7 @@ func (m *Manager) GetConfig() ([]proto.AgentConfig, []error) {
 }
 
 func (m *Manager) Handle(cmd *proto.Cmd) *proto.Reply {
+	// MRMS doesn't handle any cmds
 	return cmd.Reply(nil, pct.UnknownCmdError{Cmd: cmd.Cmd})
 }
 
