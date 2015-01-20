654d8025bd8f5fbd55ea1e6121512d0b4ae6b657
Daniel Nichter <daniel@percona.com>
2015-01-20 13:34:16 -0700

    Add real perfschema worker test.

--- qan/perfschema/perfschema_test.go
+++ qan/perfschema/perfschema_test.go
@@ -21,17 +21,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 	"time"
 
 	. "github.com/go-test/test"
 	"github.com/percona/cloud-protocol/proto"
-	//"github.com/percona/percona-agent/mysql"
+	"github.com/percona/percona-agent/mysql"
 	"github.com/percona/percona-agent/pct"
 	"github.com/percona/percona-agent/qan"
 	"github.com/percona/percona-agent/qan/perfschema"
-	"github.com/percona/percona-agent/test"
+	//"github.com/percona/percona-agent/test"
 	"github.com/percona/percona-agent/test/mock"
 	. "gopkg.in/check.v1"
 )
@@ -43,6 +44,7 @@ var inputDir = RootDir() + "/test/qan/perfschema/"
 var outputDir = RootDir() + "/test/qan/perfschema/"
 
 type WorkerTestSuite struct {
+	dsn       string
 	logChan   chan *proto.LogEntry
 	logger    *pct.Logger
 	nullmysql *mock.NullMySQL
@@ -51,6 +53,7 @@ type WorkerTestSuite struct {
 var _ = Suite(&WorkerTestSuite{})
 
 func (s *WorkerTestSuite) SetUpSuite(t *C) {
+	s.dsn = os.Getenv("PCT_TEST_MYSQL_DSN")
 	s.logChan = make(chan *proto.LogEntry, 100)
 	s.logger = pct.NewLogger(s.logChan, "qan-worker")
 	s.nullmysql = mock.NewNullMySQL()
@@ -166,8 +169,7 @@ func (s *WorkerTestSuite) Test001(t *C) {
 	t.Assert(err, IsNil)
 }
 
-/*
-func (s *PfsWorkerTestSuite) TestCollectData(t *C) {
+func (s *WorkerTestSuite) TestRealWorker(t *C) {
 	if s.dsn == "" {
 		t.Fatal("PCT_TEST_MYSQL_DSN is not set")
 	}
@@ -176,156 +178,78 @@ func (s *PfsWorkerTestSuite) TestCollectData(t *C) {
 	t.Assert(err, IsNil)
 	defer mysqlConn.Close()
 
-	enablePfs := []mysql.Query{
+	f := perfschema.NewRealWorkerFactory(s.logChan)
+	w := f.Make("qan-worker", mysqlConn)
+
+	start := []mysql.Query{
 		mysql.Query{Verify: "performance_schema", Expect: "1"},
 		mysql.Query{Set: "UPDATE performance_schema.setup_consumers SET ENABLED = 'YES' WHERE NAME = 'statements_digest'"},
 		mysql.Query{Set: "UPDATE performance_schema.setup_instruments SET ENABLED = 'YES', TIMED = 'YES' WHERE NAME LIKE 'statement/sql/%'"},
 		mysql.Query{Set: "TRUNCATE performance_schema.events_statements_summary_by_digest"},
 	}
-	if err := mysqlConn.Set(enablePfs); err != nil {
+	if err := mysqlConn.Set(start); err != nil {
 		t.Fatal(err)
 	}
-
-	db := mysqlConn.DB()
-	_, err = db.Exec("SELECT NOW()")
-	_, err = db.Exec("SELECT 1")
-	_, err = db.Exec("SELECT * FROM `events_statements_summary_by_digest`")
-
-	 // as we don't have consistent order in maps from Go v1.3,
-	 //let's use pre-defined map with queries
-	expectedResult := make(map[string]bool)
-	expectedResult["TRUNCATE `performance_schema` . `events_statements_summary_by_digest` "] = true
-	expectedResult["SELECT NOW ( ) "] = true
-	expectedResult["SELECT ? "] = true
-	expectedResult["SELECT * FROM `events_statements_summary_by_digest` "] = true
-
-	w := qan.NewPfsWorker(s.logger, "pfs-worker", mysqlConn)
-	gotPfsData, err := w.CollectData()
-	t.Assert(err, IsNil)
-	t.Assert(gotPfsData, NotNil)
-	for i := range gotPfsData {
-		if !expectedResult[gotPfsData[i].DigestText] {
-			t.Errorf("Missing %s", gotPfsData[i].DigestText)
-			Dump(gotPfsData)
+	stop := []mysql.Query{
+		mysql.Query{Set: "UPDATE performance_schema.setup_consumers SET ENABLED = 'NO' WHERE NAME = 'statements_digest'"},
+		mysql.Query{Set: "UPDATE performance_schema.setup_instruments SET ENABLED = 'NO', TIMED = 'NO' WHERE NAME LIKE 'statement/sql/%'"},
+	}
+	defer func() {
+		if err := mysqlConn.Set(stop); err != nil {
+			t.Fatal(err)
 		}
-	}
-}
+	}()
 
-func (s *PfsWorkerTestSuite) TestPrepareResult001(t *C) {
-	parsedTime, _ := time.Parse("2006-01-02T15:04:05Z", "2014-07-10T19:14:30Z")
-	pfsData := []*qan.PfsRow{
-		{
-			Digest:                  "d082a30b349166452cd1148310124d77",
-			DigestText:              "TRUNCATE `events_statements_summary_by_digest` ",
-			SumTimerWait:            588631000,
-			MinTimerWait:            588631000,
-			AvgTimerWait:            588631000,
-			MaxTimerWait:            588631000,
-			SumLockTime:             119000000,
-			SumRowsAffected:         0,
-			SumRowsSent:             0,
-			SumRowsExamined:         0,
-			SumSelectFullJoin:       0,
-			SumSelectScan:           0,
-			SumSortMergePasses:      0,
-			SumCreatedTmpDiskTables: 0,
-			SumCreatedTmpTables:     0,
-			CountStar:               1,
-			FirstSeen:               parsedTime,
-			LastSeen:                parsedTime,
-		},
-		{
-			Digest:                  "973f7f10f95fc62e80148f2845ceca42",
-			DigestText:              "SELECT NOW ( ) ",
-			SumTimerWait:            41687000,
-			MinTimerWait:            41687000,
-			AvgTimerWait:            41687000,
-			MaxTimerWait:            41687000,
-			SumLockTime:             0,
-			SumRowsAffected:         0,
-			SumRowsSent:             1,
-			SumRowsExamined:         0,
-			SumSelectFullJoin:       0,
-			SumSelectScan:           0,
-			SumSortMergePasses:      0,
-			SumCreatedTmpDiskTables: 0,
-			SumCreatedTmpTables:     0,
-			CountStar:               1,
-			FirstSeen:               parsedTime,
-			LastSeen:                parsedTime,
-		},
-		{
-			Digest:                  "93eaedb019bdfcf59f7aea8a25486ef0",
-			DigestText:              "SELECT ? ",
-			SumTimerWait:            20274000,
-			MinTimerWait:            20274000,
-			AvgTimerWait:            20274000,
-			MaxTimerWait:            20274000,
-			SumLockTime:             0,
-			SumRowsAffected:         0,
-			SumRowsSent:             1,
-			SumRowsExamined:         0,
-			SumSelectFullJoin:       0,
-			SumSelectScan:           0,
-			SumSortMergePasses:      0,
-			SumCreatedTmpDiskTables: 0,
-			SumCreatedTmpTables:     0,
-			CountStar:               1,
-			FirstSeen:               parsedTime,
-			LastSeen:                parsedTime,
-		},
-		{
-			Digest:                  "fcc2b877639138358ef059551099f0d0",
-			DigestText:              "SELECT * FROM `events_statements_summary_by_digest` ",
-			SumTimerWait:            155949000,
-			MinTimerWait:            155949000,
-			AvgTimerWait:            155949000,
-			MaxTimerWait:            155949000,
-			SumLockTime:             38000000,
-			SumRowsAffected:         0,
-			SumRowsSent:             3,
-			SumRowsExamined:         3,
-			SumSelectFullJoin:       0,
-			SumSelectScan:           1,
-			SumSortMergePasses:      0,
-			SumCreatedTmpDiskTables: 0,
-			SumCreatedTmpTables:     0,
-			CountStar:               1,
-			FirstSeen:               parsedTime,
-			LastSeen:                parsedTime,
-		},
-		{
-			Digest:                  "2ea7017783cf24845827fd4e2cff1a5b",
-			DigestText:              "USE `performance_schema` ",
-			SumTimerWait:            24017000,
-			MinTimerWait:            24017000,
-			AvgTimerWait:            24017000,
-			MaxTimerWait:            24017000,
-			SumLockTime:             0,
-			SumRowsAffected:         0,
-			SumRowsSent:             0,
-			SumRowsExamined:         0,
-			SumSelectFullJoin:       0,
-			SumSelectScan:           0,
-			SumSortMergePasses:      0,
-			SumCreatedTmpDiskTables: 0,
-			SumCreatedTmpTables:     0,
-			CountStar:               1,
-			FirstSeen:               parsedTime,
-			LastSeen:                parsedTime,
-		},
-	}
+	// SCHEMA_NAME: NULL
+	//      DIGEST: fbe070dfb47e4a2401c5be6b5201254e
+	// DIGEST_TEXT: SELECT ? FROM DUAL
+	_, err = mysqlConn.DB().Exec("SELECT 'teapot' FROM DUAL")
 
-	w := qan.NewPfsWorker(s.logger, "pfs-worker", mock.NewNullMySQL())
-	got, err := w.PrepareResult(pfsData)
+	// First interval.
+	err = w.Setup(&qan.Interval{Number: 1, StartTime: time.Now().UTC()})
 	t.Assert(err, IsNil)
-	t.Assert(got, NotNil)
-	expect := &qan.Result{}
-	err = test.LoadMmReport(outputDir+"pfs001.json", expect)
+
+	res, err := w.Run()
 	t.Assert(err, IsNil)
-	if ok, diff := IsDeeply(got, expect); !ok {
-		Dump(got)
-		t.Error(diff)
+	t.Check(res, IsNil)
+
+	err = w.Cleanup()
+	t.Assert(err, IsNil)
+
+	// Some query activity between intervals.
+	_, err = mysqlConn.DB().Exec("SELECT 'teapot' FROM DUAL")
+	time.Sleep(1 * time.Second)
+
+	// Second interval and a result.
+	err = w.Setup(&qan.Interval{Number: 1, StartTime: time.Now().UTC()})
+	t.Assert(err, IsNil)
+
+	res, err = w.Run()
+	t.Assert(res, NotNil)
+	if len(res.Class) == 0 {
+		t.Fatal("Expected len(res.Class) > 0")
 	}
+	t.Check(res.Class, HasLen, 1)
+	class := res.Class[0]
+	t.Check(class.Id, Equals, "01C5BE6B5201254E")
+	t.Check(class.Fingerprint, Equals, "SELECT ? FROM DUAL ")
+	queryTime := class.Metrics.TimeMetrics["Query_time"]
+	if queryTime.Min == 0 {
+		t.Error("Expected Query_time_min > 0")
+	}
+	if queryTime.Max == 0 {
+		t.Error("Expected Query_time_max > 0")
+	}
+	if queryTime.Avg == 0 {
+		t.Error("Expected Query_time_avg > 0")
+	}
+	if queryTime.Min > queryTime.Max {
+		t.Error("Expected Query_time_min >= Query_time_max")
+	}
+	t.Check(class.Metrics.NumberMetrics["Rows_affected"].Sum, Equals, uint64(0))
+	t.Check(class.Metrics.NumberMetrics["Rows_examined"].Sum, Equals, uint64(0))
+	t.Check(class.Metrics.NumberMetrics["Rows_sent"].Sum, Equals, uint64(1))
+
+	err = w.Cleanup()
+	t.Assert(err, IsNil)
 }
-*/
--- qan/perfschema/worker.go
+++ qan/perfschema/worker.go
@@ -53,11 +53,15 @@ type DigestRow struct {
 	LastSeen                time.Time
 }
 
+// A Class represents a single query and its per-schema instances.
 type Class struct {
 	DigestText string
 	Rows       map[string]*DigestRow // keyed on schema
 }
 
+// A Snapshot represents all rows from performance_schema.events_statements_summary_by_digest
+// at a single time, grouped by digest into classes. Two consecutive Snapshots are needed to
+// produce a qan.Result.
 type Snapshot map[string]Class // keyed on digest (classId)
 
 // --------------------------------------------------------------------------
@@ -161,9 +165,9 @@ type Worker struct {
 	// --
 	name   string
 	status *pct.Status
-	sync   *pct.SyncChan
 	prev   Snapshot
 	curr   Snapshot
+	iter   *qan.Interval
 }
 
 func NewWorker(logger *pct.Logger, mysqlConn mysql.Connector, getRows GetDigestRowsFunc, getText GetDigestTextFunc) *Worker {
@@ -176,19 +180,19 @@ func NewWorker(logger *pct.Logger, mysqlConn mysql.Connector, getRows GetDigestR
 		// --
 		name:   name,
 		status: pct.NewStatus([]string{name}),
-		sync:   pct.NewSyncChan(),
 		prev:   make(Snapshot),
 	}
 	return w
 }
 
 func (w *Worker) Setup(interval *qan.Interval) error {
+	w.iter = interval
 	return nil
 }
 
 func (w *Worker) Run() (*qan.Result, error) {
-	w.logger.Debug("Run:call")
-	defer w.logger.Debug("Run:return")
+	w.logger.Debug("Run:call:", w.iter.Number)
+	defer w.logger.Debug("Run:return:", w.iter.Number)
 
 	if err := w.mysqlConn.Connect(1); err != nil {
 		w.logger.Warn("Cannot connect to MySQL:", err)
@@ -196,21 +200,22 @@ func (w *Worker) Run() (*qan.Result, error) {
 	}
 	defer w.mysqlConn.Close()
 	var err error
-	w.curr, err = w.GetSnapshot(w.prev)
+	w.curr, err = w.getSnapshot(w.prev)
 	if err != nil {
 		return nil, err
 	}
-	return w.PrepareResult(w.prev, w.curr)
+	return w.prepareResult(w.prev, w.curr)
 }
 
 func (w *Worker) Cleanup() error {
+	w.logger.Debug("Cleanup:call:", w.iter.Number)
+	defer w.logger.Debug("Cleanup:return:", w.iter.Number)
+	w.status.Update(w.name, "Idle")
 	w.prev = w.curr
 	return nil
 }
 
 func (w *Worker) Stop() error {
-	w.sync.Stop()
-	w.sync.Wait()
 	return nil
 }
 
@@ -220,7 +225,10 @@ func (w *Worker) Status() map[string]string {
 
 // --------------------------------------------------------------------------
 
-func (w *Worker) GetSnapshot(prev Snapshot) (Snapshot, error) {
+func (w *Worker) getSnapshot(prev Snapshot) (Snapshot, error) {
+	w.logger.Debug("getSnapshot:call:", w.iter.Number)
+	defer w.logger.Debug("getSnapshot:return:", w.iter.Number)
+
 	w.status.Update(w.name, "Processing rows")
 	defer w.status.Update(w.name, "Idle")
 
@@ -235,7 +243,6 @@ func (w *Worker) GetSnapshot(prev Snapshot) (Snapshot, error) {
 	for row := range rowChan {
 		classId := strings.ToUpper(row.Digest[16:32])
 		if class, haveClass := curr[classId]; haveClass {
-			fmt.Println("have class in curr", classId)
 			if _, haveRow := class.Rows[row.Schema]; haveRow {
 				w.logger.Error("Got class twice: ", row.Schema, row.Digest)
 				continue
@@ -246,11 +253,9 @@ func (w *Worker) GetSnapshot(prev Snapshot) (Snapshot, error) {
 			var digestText string
 			if prevClass, havePrevClass := prev[classId]; havePrevClass {
 				// Class was in previous iter, so re-use its digest text.
-				fmt.Println("class in prev", classId)
 				digestText = prevClass.DigestText
 			} else {
 				// Have never seen class before, so get digext text from perf schema.
-				fmt.Println("never seen class", classId)
 				var err error
 				digestText, err = w.getText(row.Digest)
 				if err != nil {
@@ -270,7 +275,10 @@ func (w *Worker) GetSnapshot(prev Snapshot) (Snapshot, error) {
 	return curr, nil
 }
 
-func (w *Worker) PrepareResult(prev, curr Snapshot) (*qan.Result, error) {
+func (w *Worker) prepareResult(prev, curr Snapshot) (*qan.Result, error) {
+	w.logger.Debug("prepareResult:call:", w.iter.Number)
+	defer w.logger.Debug("prepareResult:return:", w.iter.Number)
+
 	w.status.Update(w.name, "Preparing result")
 	defer w.status.Update(w.name, "Idle")
 
@@ -284,7 +292,6 @@ CLASS_LOOP:
 		// If this class does not exist in prev, skip the entire class.
 		prevClass, ok := prev[classId]
 		if !ok {
-			fmt.Println(classId, "not in prev, skip")
 			continue CLASS_LOOP
 		}
 
@@ -301,20 +308,17 @@ CLASS_LOOP:
 			// the first time we've seen this query in this schema.
 			prevRow, ok := prevClass.Rows[schema]
 			if !ok {
-				fmt.Println(classId, schema, "not in prev, skip")
 				continue ROW_LOOP
 			}
 
 			// If query count has not changed, skip it. This means the query was
 			// not executed between prev and curr interval.
 			if row.CountStar == prevRow.CountStar {
-				fmt.Println(classId, schema, "not executed, skip")
 				continue ROW_LOOP
 			}
 
 			// This per-schema query exists in both prev and curr, so add the diff
 			// of its values to the class aggregate.
-			fmt.Println(classId, schema, "diff")
 			n++
 
 			// Add the diff of the totals to the class metric totals. For example,
@@ -343,6 +347,11 @@ CLASS_LOOP:
 			}
 			// Add the averages, divide later.
 			d.AvgTimerWait += row.AvgTimerWait
+		}
+
+		// Class was in prev, but no rows in prev were in curr, so skip the class.
+		if n == 0 {
+			continue CLASS_LOOP
 		}
 
 		// Divide the total averages to yield the average of the averages.
