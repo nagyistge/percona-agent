7faacd80349cdb2d11ba40d6ca1e283c247b3fdb
Carlos Salguero <cfsalguero@gmail.com>
2015-02-10 09:49:40 -0300

    PCT-1054 EXPLAIN non-SELECT queries
    Added a new method to convert DML queries to their equivalent SELECT.

--- query/service/explain.go
+++ query/service/explain.go
@@ -20,6 +20,9 @@ package service
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
+	"strings"
+
 	"github.com/percona/cloud-protocol/proto"
 	"github.com/percona/percona-agent/instance"
 	"github.com/percona/percona-agent/mysql"
@@ -76,7 +79,16 @@ func (e *Explain) Handle(cmd *proto.Cmd) *proto.Reply {
 	// Run explain
 	explain, err := conn.Explain(explainQuery.Query, explainQuery.Db)
 	if err != nil {
-		return cmd.Reply(nil, fmt.Errorf("Explain failed for %s: %s", name, err))
+		if e.isDMLQuery(explainQuery.Query) {
+			newQuery := e.DMLToSelect(explainQuery.Query)
+			if newQuery == "" {
+				return cmd.Reply(nil, fmt.Errorf("Cannot run EXPLAIN on selected query"))
+			}
+			explain, err = conn.Explain(e.DMLToSelect(explainQuery.Query), explainQuery.Db)
+		}
+		if err != nil {
+			return cmd.Reply(nil, fmt.Errorf("Explain failed for %s: %s", name, err))
+		}
 	}
 
 	return cmd.Reply(explain)
@@ -116,4 +128,94 @@ func (e *Explain) getExplainQuery(cmd *proto.Cmd) (explainQuery *proto.ExplainQu
 	}
 
 	return explainQuery, nil
+}
+
+func (e *Explain) isDMLQuery(query string) bool {
+	query = strings.ToLower(strings.TrimSpace(query))
+	dmlVerbs := []string{"insert", "update", "delete", "replace"}
+	for _, verb := range dmlVerbs {
+		if strings.HasPrefix(query, verb) {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+  MySQL version prior 5.6.3 cannot run explain on DML commands.
+  From the doc: http://dev.mysql.com/doc/refman/5.6/en/explain.html
+  "As of MySQL 5.6.3, permitted explainable statements for EXPLAIN are
+  SELECT, DELETE, INSERT, REPLACE, and UPDATE.
+  Before MySQL 5.6.3, SELECT is the only explainable statement."
+
+  This function converts DML queries to the equivalent SELECT to make
+  it able to explain DML queries on older MySQL versions
+*/
+func (e *Explain) DMLToSelect(query string) string {
+	var updateRe = regexp.MustCompile(`(?i)^update\s+(?:low_priority|ignore)?\s*(.*?)\s+set\s+(.*?)(?:\s+where\s+(.*?))?(?:\s+limit\s*[0-9]+(?:\s*,\s*[0-9]+)?)?$`)
+	var deleteRe = regexp.MustCompile(`(?i)^delete\s+(.*?)\bfrom\s+(.*?)$`)
+	var insertRe = regexp.MustCompile(`(?i)^(?:insert(?:\s+ignore)?|replace)\s+.*?\binto\s+(.*?)\(([^\)]+)\)\s*values?\s*\((.*?)\)\s*(?:\slimit\s|on\s+duplicate\s+key.*)?\s*$`)
+	var insertSetRe = regexp.MustCompile(`(?i)(?:insert(?:\s+ignore)?|replace)\s+(?:.*?\binto)\s+(.*?)\s*set\s+(.*?)\s*(?:\blimit\b|on\s+duplicate\s+key.*)?\s*$`)
+
+	m := updateRe.FindStringSubmatch(query)
+	// > 2 because we need at least a table name and a list of fields
+	if len(m) > 2 {
+		return updateToSelect(m)
+	}
+
+	m = deleteRe.FindStringSubmatch(query)
+	if len(m) > 1 {
+		return deleteToSelect(m)
+	}
+
+	m = insertRe.FindStringSubmatch(query)
+	if len(m) > 2 {
+		return insertToSelect(m)
+	}
+
+	m = insertSetRe.FindStringSubmatch(query)
+	if len(m) > 2 {
+		return insertWithSetToSelect(m)
+	}
+
+	return ""
+}
+
+func updateToSelect(matches []string) string {
+	matches = matches[1:]
+	matches[0], matches[1] = matches[1], matches[0]
+	format := []string{"SELECT %s", " FROM %s", " WHERE %s"}
+	result := ""
+	for i, match := range matches {
+		if match != "" {
+			result = result + fmt.Sprintf(format[i], match)
+		}
+	}
+	return result
+}
+
+func deleteToSelect(matches []string) string {
+	if strings.Index(matches[2], "join") > -1 {
+		return fmt.Sprintf("SELECT 1 FROM %s", matches[2])
+	}
+	return fmt.Sprintf("SELECT * FROM %s", matches[2])
+}
+
+func insertToSelect(matches []string) string {
+	fields := strings.Split(matches[2], ",")
+	values := strings.Split(matches[3], ",")
+	if len(fields) == len(values) {
+		query := fmt.Sprintf("SELECT * FROM %s WHERE ", matches[1])
+		sep := ""
+		for i := 0; i < len(fields); i++ {
+			query = query + fmt.Sprintf(`%s%s="%s"`, sep, strings.TrimSpace(fields[i]), values[i])
+			sep = " and "
+		}
+		return query
+	}
+	return fmt.Sprintf("SELECT * FROM %s LIMIT 1", matches[1])
+}
+
+func insertWithSetToSelect(matches []string) string {
+	return fmt.Sprintf("SELECT * FROM %s WHERE %s", matches[1], strings.Replace(matches[2], ",", " AND ", -1))
 }
