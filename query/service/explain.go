bf9b3274d6bdea1ecd1492928eaf0eb1b8e73217
Carlos Salguero <carlos.salguero@percona.com>
2015-04-02 16:28:20 -0300

    PCT-1510 Fixed params parsing for DML 2 SELECT

--- query/service/explain.go
+++ query/service/explain.go
@@ -217,7 +217,7 @@ func insertToSelect(matches []string) string {
 		query := fmt.Sprintf("SELECT * FROM %s WHERE ", matches[1])
 		sep := ""
 		for i := 0; i < len(fields); i++ {
-			query = query + fmt.Sprintf(`%s%s="%s"`, sep, strings.TrimSpace(fields[i]), values[i])
+			query = query + fmt.Sprintf(`%s%s=%s`, sep, strings.TrimSpace(fields[i]), values[i])
 			sep = " and "
 		}
 		return query
